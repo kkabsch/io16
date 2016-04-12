6c2d4c81f5d395905bebf10daa7b14bb7d387b7a
kkabsch <kabsch.k@gmail.com>
2016-04-12 22:43:40 +0200

    Poprawka funkcji

--- io16/kalkulator.py
+++ io16/kalkulator.py
@@ -3,5 +3,13 @@ def dodawanie():
 	b=int(input("podaj b"))
 	print (a+b)
 
+def odejmowanie():
+	a=int(input("podaj a"))
+	b=int(input("podaj b"))
+	print (a-b)
+
 dodawanie()
+odejmowanie()
 input()
+
+
